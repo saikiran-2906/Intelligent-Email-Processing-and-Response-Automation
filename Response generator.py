eac7c452d5967a6e87af8a9fe0f7ba1366fe8bfc
myadasrikar-30 <myadasrikar@gmail.com>
2026-02-10 09:30:24 +0530

    main.py

--- Response generator.py
+++ Response generator.py
@@ -1,13 +1,10 @@
-class ResponseGenerator:
+classifier = EmailClassifier()
+responder = ResponseGenerator()
 
-    def __init__(self):
-        self.templates = {
-            "Order": "Your order is being processed. Thank you for your patience.",
-            "Refund": "Your refund request has been received. We will update you shortly.",
-            "Account": "Please use the password reset link to recover your account.",
-            "Complaint": "We are sorry for the inconvenience. Our team will contact you.",
-            "General": "Thank you for contacting support. We will respond soon."
-        }
+email = input("Enter email text: ")
 
-    def generate_response(self, category):
-        return self.templates.get(category, self.templates["General"])
+category = classifier.classify(email)
+response = responder.generate_response(category)
+
+print("Detected Category:", category)
+print("Auto Response:", response)
